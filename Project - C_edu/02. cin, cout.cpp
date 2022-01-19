dd14ab4b4013f027b9f2723626cc20d0f0e86b64
egg_jini <egg.design@daum.net>
2022-01-19 12:38:07 +0900

    2nd lecture commint

---
+++ Project - C_edu/02. cin, cout.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//int age = 22;
+	//cout << endl << "당신의 나이는 " << age << "살이네요" << endl;
+
+	int age;
+	cout << endl << "당신의 나이를 입력하세요 >> ";
+	cin >> age;
+	cout << endl << "당신의 나이는 " << age << "살이네요" << endl;
+}
