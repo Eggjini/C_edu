935c5640cd286290c7c54f2dc190ce9131e99fb6
egg_jini <egg.design@daum.net>
2022-01-19 14:18:14 +0900

    Update 02. cin, cout.cpp

--- 02. cin, cout.cpp
+++ 02. cin, cout.cpp
@@ -5,8 +5,12 @@ int main()
 	//int age = 22;
 	//cout << endl << "당신의 나이는 " << age << "살이네요" << endl;
 
-	int age;
-	cout << endl << "당신의 나이를 입력하세요 >> ";
-	cin >> age;
-	cout << endl << "당신의 나이는 " << age << "살이네요" << endl;
+	//int age;
+	//cout << endl << "당신의 나이를 입력하세요 >> ";
+	//cin >> age;
+	//cout << endl << "당신의 나이는 " << age << "살이네요" << endl;
+
+	//string 자료형
+	string name = "egg_jini";
+	cout << name << endl;
 }
